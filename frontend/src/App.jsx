6943564484f1bb40427493ed1748bdcde46935b9
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-06 16:52:07 +0530

    create the header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
