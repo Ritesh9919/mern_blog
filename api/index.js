f88781e6655dd3410e4e2181c4ae8811d71a7ba0
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-07 10:03:31 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./config/database.js";
+import errorHandlerMiddleware from "./middlewares/errorHandler.middleware.js";
+import notFoundMiddleware from "./middlewares/notFound.middleware.js";
+
+import userRouter from "./routes/user.route.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use("/api/users", userRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 connectDB()
   .then(() => {
