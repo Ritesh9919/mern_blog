350e8547287d3b3d0fb5841ce0532c8cbc26dd44
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-08 11:00:03 +0530

    complete dark mode functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,14 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./redux/store.js";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 );
