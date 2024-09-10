dc6816f86be69f4f0a21b8dca592f41c069adaa7
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-10 16:10:09 +0530

    create getPost api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import { title } from "process";
 import { Post } from "../models/post.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -24,6 +25,51 @@ export const createPost = async (req, res, next) => {
       .status(201)
       .json(new ApiResponse(true, "Post created successfully", post));
   } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    return res.status(200).json(
+      new ApiResponse(true, "Posts feched successfully", {
+        posts,
+        totalPosts,
+        lastMonthPosts,
+      })
+    );
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -2,7 +2,8 @@ import express from "express";
 const router = express.Router();
 
 import verifyJwt from "../middlewares/auth.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 router.post("/create", verifyJwt, createPost);
+router.get("/getPosts", getPosts);
 
 export default router;
