699d569f5a7f8defc250db402b41c2f1a86e09cd
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-12 12:35:48 +0530

    complete post page functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
             <Route path="/create-post" element={<CreatePost />} />
             <Route path="/update-post/:postId" element={<UpdatePost />} />
           </Route>
+          <Route path="/post/:postSlug" element={<PostPage />} />
         </Routes>
         <Footer />
       </Router>
